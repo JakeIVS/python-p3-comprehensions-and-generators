1d1b993609bf79de74b28090737987e6cb87817c
Jacob Ives <jacob.w.ives@gmail.com>
2023-06-12 16:30:45 -0600

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,11 @@
 
 def return_evens(num_list):
     pass
+    evens_list = []
+    for n in num_list:
+        if not n % 2:
+            evens_list.append(n)
+    return evens_list
 
 def make_exclamation(sentence_list):
-    pass
+    return [string + "!" for string in sentence_list]
